79cb4085ed15e14200002c828147ead7dee7bc28
이선우 <60869316+sunwoo0706@users.noreply.github.com>
2021-11-25 01:10:20 +0900

    더하기 : 뿌리부분에 띄움창 제공자 더하기

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import 뿌리 from "컴포넌트들/뿌리";
 import React from "react";
 import { RecoilRoot as 리코일제공자 } from "recoil";
 import 리액트제공자 from "react-dom";
+import 띄움창제공자 from "컴포넌트들/띄움창/띄움창제공자";
 import 글로벌스타일 from "스타일들/글로벌스타일";
 import { BrowserRouter as 브라우저라우터 } from "react-router-dom";
 
@@ -9,6 +10,7 @@ import { BrowserRouter as 브라우저라우터 } from "react-router-dom";
   <리코일제공자>
     <브라우저라우터>
       <뿌리 />
+      <띄움창제공자 />
       <글로벌스타일 />
     </브라우저라우터>
   </리코일제공자>,
