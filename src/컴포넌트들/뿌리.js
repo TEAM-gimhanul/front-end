cdb9723fb05db202c0d188ba06cc9cc9cac095b8
4YJ <ohman0705@naver.com>
2021-11-24 19:40:38 +0900

    추가: 선두

--- src/컴포넌트들/뿌리.js
+++ src/컴포넌트들/뿌리.js
@@ -2,11 +2,13 @@ import React from "react";
 import { Route as 라우트, Routes as 라우트들 } from "react-router";
 import 담소항목 from "./담소/담소항목";
 import 모서리막대 from "./모서리막대/사람목록";
+import 선두 from "./선두/선두";
 const 뿌리 = () => {
   return (
     <라우트들>
       <라우트 path="/t1" element={<모서리막대 />} />
       <라우트 path="/test" element={<담소항목 />} />
+      <라우트 path="header" element={<선두 />} />
     </라우트들>
   );
 };
