8fc7aa9d489dc480cef7bc29f8af1ad14e8d240a
이선우 <60869316+sunwoo0706@users.noreply.github.com>
2021-11-25 01:09:42 +0900

    더하기 : 띄움창제공자

---
+++ src/컴포넌트들/띄움창/띄움창제공자.jsx
@@ -0,0 +1,38 @@
+import { useRecoilValue as 전역상태관리값 } from "recoil";
+import 꾸미기 from "styled-components";
+import { 띄움창전역상태분자 } from "상태저장소/상태분자";
+import 입장띄움창 from "./띄움창들/입장띄움창";
+import 회원가입띄움창 from "./띄움창들/회원가입띄움창";
+
+export const 띄움창보자기 = 꾸미기.div`
+  width: 700px;
+  height: 500px;
+  z-index: 100;
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background: #fff;
+  border: 1px solid black;
+  border-radius: 30px;
+  padding: 20px;
+`;
+
+const 띄움창들 = {
+  입장띄움창: <입장띄움창 />,
+  회원가입띄움창: <회원가입띄움창 />,
+};
+
+const 띄움창제공자 = () => {
+  const 띄움창상태 = 전역상태관리값(띄움창전역상태분자);
+
+  const 띄움창상태생성 = () => {
+    if (!띄움창상태) return;
+
+    return 띄움창들[띄움창상태];
+  };
+
+  return <>{띄움창상태생성()}</>;
+};
+
+export default 띄움창제공자;
