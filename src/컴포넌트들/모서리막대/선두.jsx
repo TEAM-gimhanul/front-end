c81d0f90850a578fb0e869bca233833b361e29d6
이선우 <60869316+sunwoo0706@users.noreply.github.com>
2021-11-25 12:28:13 +0900

    더하기 : 외부인증띄움창 띄우기

--- src/컴포넌트들/모서리막대/선두.jsx
+++ src/컴포넌트들/모서리막대/선두.jsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import 주제받기 from "잡동사니/주제받기";
+import 띄움창갈고리 from "갈고리들/띄움창갈고리";
 
 const 보자기 = styled.div`
   width: 344px;
@@ -12,7 +13,7 @@ const 보자기 = styled.div`
   padding: 6px 14px;
   flex-direction: row;
   background-color: ${({ theme }) => theme.색깔들.white};
-  box-shadow: 0 17px 20px -18px ${(props) => (props.주제 ? props.theme.색깔들.gray800 : props.theme.색깔들.gray200)};
+  box-shadow: 0 17px 20px -18px ${props => (props.주제 ? props.theme.색깔들.gray800 : props.theme.색깔들.gray200)};
   @media only screen and (max-width: 900px) {
     width: 72px;
     min-height: 144px;
@@ -60,10 +61,14 @@ const 단추 = styled.div`
 
 const 선두 = () => {
   const 현재주제 = 주제받기();
+  const { 띄움창나타내기 } = 띄움창갈고리();
+  const 외부인증띄움창조작 = () => {
+    띄움창나타내기("외부인증띄움창");
+  };
   return (
     <보자기 주제={현재주제}>
       <단추보자기>
-        <단추>로그인</단추>
+        <단추 onClick={() => 외부인증띄움창조작()}>로그인</단추>
         <단추>랜덤매칭</단추>
       </단추보자기>
     </보자기>
