5d01b579f1149479dad378295a9ee93f8c22a171
이선우 <60869316+sunwoo0706@users.noreply.github.com>
2021-11-25 12:17:47 +0900

    더하기 : 외부인증띄움창

---
+++ src/컴포넌트들/띄움창/띄움창들/외부인증띄움창.jsx
@@ -0,0 +1,51 @@
+import { 띄움창보자기 } from "../띄움창제공자";
+import 꾸미기 from "styled-components";
+
+const 외부인증띄움창헤더 = 꾸미기.h3`
+  font-size: 1.5rem;
+  font-weight: bold;
+  margin-bottom: 1rem;
+`;
+
+const 외부인증띄움창꾸미기 = 꾸미기.div`
+  width: 340px;
+  height: 300px;
+  background-color: #fff;
+  border-radius: 10px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.4);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const 외부인증요소 = 꾸미기.button`
+  width: 80%;
+  margin: 0 auto;
+  & + & {
+    margin-top: 20px;
+  }
+  padding: 20px;
+  border: none;
+  border-radius: 10px;
+  background-color: #f5f5f5;
+  font-size: 14px;
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+const 외부인증띄움창 = () => {
+  return (
+    <띄움창보자기>
+      <외부인증띄움창꾸미기>
+        <외부인증띄움창헤더>외부인증</외부인증띄움창헤더>
+        <외부인증요소>카카오</외부인증요소>
+        <외부인증요소>네이버</외부인증요소>
+        <외부인증요소>구글</외부인증요소>
+      </외부인증띄움창꾸미기>
+    </띄움창보자기>
+  );
+};
+
+export default 외부인증띄움창;
