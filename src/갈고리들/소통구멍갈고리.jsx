856fb638a90479f1beb84284ed10321249cb4996
Eungyeol <eungyeole@naver.com>
2021-11-25 13:54:41 +0900

    추가: 환경변수

--- src/갈고리들/소통구멍갈고리.jsx
+++ src/갈고리들/소통구멍갈고리.jsx
@@ -1,15 +1,13 @@
 import { useEffect, useRef } from "react";
 import 소통구멍 from "socket.io-client";
+import { 기본_끝점 } from "구성들/구성";
 
 const 소통구멍갈고리 = () => {
   const 소통구멍참조 = useRef();
   useEffect(() => {
-    소통구멍참조.current = 소통구멍.connect(
-      "https://eb8sspxdcc.execute-api.ap-northeast-2.amazonaws.com",
-      {
-        transports: ["websocket"],
-      }
-    );
+    소통구멍참조.current = 소통구멍.connect(기본_끝점, {
+      transports: ["websocket"],
+    });
   }, [소통구멍참조]);
 
   return 소통구멍참조.current;
