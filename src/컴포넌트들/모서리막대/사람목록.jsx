710ce12f9c6bccd70887a4ba8e8159cf42260740
Clzzi <clzzi1109@naver.com>
2021-11-25 18:22:37 +0900

    수정: 수정

--- src/컴포넌트들/모서리막대/사람목록.jsx
+++ src/컴포넌트들/모서리막대/사람목록.jsx
@@ -1,12 +1,8 @@
 import 꾸미기 from "styled-components";
 import 사람 from "컴포넌트들/모서리막대/사람";
 import 선두 from "컴포넌트들/모서리막대/선두";
-import 소통구멍갈고리 from "갈고리들/소통구멍갈고리";
 import { useEffect } from "react";
-import { 기본_끝점 } from "구성들/구성";
-import axios from "axios";
-import { 사용자상태분자 } from "상태저장소/상태분자";
-import { useRecoilState as 상태총괄갈고리 } from "recoil";
+import use사용자목록갈고리 from "갈고리들/사용자목록갈고리";
 
 const 모서리막대테두리 = 꾸미기.div`
   display: inline-flex;
@@ -28,24 +24,10 @@ const 모서리막대테두리 = 꾸미기.div`
 `;
 
 const 사람목록 = ({ 소통구멍 }) => {
-  const [사용자들, 사용자들정의] = 상태총괄갈고리(사용자상태분자);
-
-  const getUserList = async () => {
-    try {
-      const { data } = await axios.get(`${기본_끝점}/rooms`, {
-        headers: {
-          Authorization: `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJjbHp6aTExMDlAbmF2ZXIuY29tIiwidHlwZSI6ImFjY2VzcyIsImlhdCI6MTYzNzgyMzcyNCwiZXhwIjoxNjM3ODMwNjI0fQ.jTDaoiX2VZ70YOEz4oZvjOnK9Y79Xhyqo032luoqrLg`,
-          "Access-Control-Allow-Origin": "*",
-        },
-      });
-      사용자들정의(data);
-    } catch (e) {
-      console.log(e);
-    }
-  };
+  const [사용자들, 사용자들정의, 유저목록받아오기] = use사용자목록갈고리();
 
   useEffect(() => {
-    getUserList();
+    유저목록받아오기();
   }, []);
 
   return (
