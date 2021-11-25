8608692e311987346aadf996a87833f7186ebbe3
Eungyeol <eungyeole@naver.com>
2021-11-25 09:49:11 +0900

    수정: 모서리막대 고정

--- src/컴포넌트들/뿌리.js
+++ src/컴포넌트들/뿌리.js
@@ -1,21 +1,31 @@
 import React from "react";
 import { Route as 라우트, Routes as 라우트들 } from "react-router";
 import TEST from "./Test";
-import 담소목록 from "./담소/담소목록";
 import 모서리막대 from "./모서리막대/사람목록";
 import 선두 from "./선두/선두";
 import 담소화면 from "../화면들/담소화면";
+import 꾸미기 from "styled-components";
 
 const 뿌리 = () => {
   return (
-    <라우트들>
-      <라우트 path="/" element={<TEST />} />
-      <라우트 path="/t1" element={<모서리막대 />} />
-      <라우트 path="/test" element={<담소항목 />} />
-      <라우트 path="header" element={<선두 />} />
-      <라우트 path="/chat" element={<담소화면 />} />
-    </라우트들>
+    <보자기>
+      <모서리막대 />
+      <라우트들>
+        <라우트 p ath="/" element={<TEST />} />
+        <라우트 path="/t1" element={<모서리막대 />} />
+        <라우트 path="/chat/:id" element={<담소화면 />} />
+      </라우트들>
+    </보자기>
   );
 };
 
 export default 뿌리;
+
+const 보자기 = 꾸미기.div`
+  display: flex;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+`;
