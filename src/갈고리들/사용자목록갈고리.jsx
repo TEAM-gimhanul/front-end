10eed4a6a97cbb50366751edcc902414de6760e0
Eungyeol <eungyeole@naver.com>
2021-11-26 04:50:16 +0900

    기능: 토스트메세지 적용

--- src/갈고리들/사용자목록갈고리.jsx
+++ src/갈고리들/사용자목록갈고리.jsx
@@ -1,7 +1,6 @@
 import { 사용자상태분자 } from "상태저장소/상태분자";
 import { useRecoilState as 상태총괄갈고리 } from "recoil";
-import { 기본_끝점 } from "구성들/구성";
-import axios from "axios";
+import 요청 from "잡동사니/요청";
 
 const use사용자목록갈고리 = () => {
   const [사용자들, 사용자들정의] = 상태총괄갈고리(사용자상태분자);
@@ -10,7 +9,7 @@ const use사용자목록갈고리 = () => {
     try {
       const token = localStorage.getItem("access_token");
       if (token) {
-        const { data } = await axios.get(`${기본_끝점}/rooms`, {
+        const { data } = await 요청.get("/rooms", {
           headers: {
             Authorization: token ? `Bearer ${token}` : null,
             "Access-Control-Allow-Origin": "*",
--- src/잡동사니/토스트.js
+++ src/잡동사니/토스트.js
@@ -1,19 +1,19 @@
-import { toast as 토스티파이 } from "react-toastify";
+import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const 토스트보여주기 = (내용, 종류) => {
   switch (종류) {
     case "성공":
-      토스티파이.success(내용, { position: toast.POSITION.TOP_RIGHT });
+      toast.success(내용, { position: toast.POSITION.TOP_RIGHT });
       break;
     case "실패":
-      토스티파이.error(내용, { position: toast.POSITION.TOP_RIGHT });
+      toast.error(내용, { position: toast.POSITION.TOP_RIGHT });
       break;
     case "정보":
-      토스티파이.info(내용, { position: toast.POSITION.TOP_RIGHT });
+      toast.info(내용, { position: toast.POSITION.TOP_RIGHT });
       break;
     default:
-      토스티파이(내용, { position: toast.POSITION.TOP_RIGHT });
+      toast(내용, { position: toast.POSITION.TOP_RIGHT });
       break;
   }
 };
